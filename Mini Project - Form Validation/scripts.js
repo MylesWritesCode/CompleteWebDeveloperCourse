89428132cec4ab7e1db1b72523bd49d636233133
Myles Berueda <myles.berueda@gmail.com>
2016-12-21 16:36:16 -1000

    Updated form.html with (minimal) styles and JS

---
+++ Mini Project - Form Validation/scripts.js
@@ -0,0 +1,22 @@
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+    return pattern.test(emailAddress);
+};
+
+$("#submitBtn").click(function(){
+  var errorMessage = "";
+  if (isValidEmailAddress($("#email").val()) == false){
+    errorMessage += "<p>Your email address is not valid.</p>";
+    // console.log(errorMessage);
+  }
+  if ($.isNumeric($("#phone").val()) == false) {
+    errorMessage += "<p>Your phone number needs to be all numbers.</p>"
+    // console.log(errorMessage);
+  }
+  if ($("#password").val() != $("#confirmPassword").val()) {
+    errorMessage += "<p>Your passwords do no match.</p>"
+    // console.log(errorMessage);
+  }
+  $("#errorMessages").html(errorMessage)
+});
