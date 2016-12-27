3803bc5826cea464cd64b7e081ab7fd61ff56275
Myles Berueda <myles.berueda@gmail.com>
2016-12-26 16:59:58 -1000

    Completed Web Scraper lesson

--- WeatherScraper/index.php
+++ WeatherScraper/index.php
@@ -1,4 +1,3 @@
-<!-- CONVERT TO PHP AFTER LAYOUT IS FINISHED -->
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,6 +6,10 @@
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="styles.css">
     <title>Weather Scraper</title>
+    <?php
+      $cityDisplay = empty($_GET["cityName"]) ? "" : $_GET["cityName"];
+      $errorMessage = "";
+    ?>
   </head>
   <body>
     <nav class="nav navbar-default navbar-fixed">
@@ -26,23 +29,54 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6 col-md-offset-3">
-          <form method="POST" class="form-group">
+          <form method="GET" class="form-group">
             <h1 class="text-center"><strong>The Weather App</strong></h1>
             <h4 class="text-center">Enter the name of a city below</h4>
             <br>
-            <input class="form-control" type="text" name="cityName">
+            <input class="form-control" type="text" name="cityName" placeholder="Eg. Honolulu, HI" value="<?php echo $cityDisplay ?>">
             <br>
             <button class="btn btn-primary btn-lg center-block">Submit</button>
           </form>
-          <div class="alert" name="weatherDescription">
-            <!-- Placeholder for code that describes weather conditions -->
-          </div>
           <?php
-            // Get data from cityName ($_POST["cityName"])
+            // Get city name and pull page from weather-forcast.com
+            $city = str_replace(' ', '', $cityDisplay);
+            // Get data from cityName ($_GET["cityName"])
+            $file_headers = @get_headers("http://www.weather-forecast.com/locations/$city/forecasts/latest");
+            if ($file_headers[0] == "HTTP/1.1 404 Not Found") {
+              $errorMessage = "That city could not be found.";
+            } else {
+              if ($city) {
+                $forecastPage = file_get_contents("http://www.weather-forecast.com/locations/$city/forecasts/latest");
+                $pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forecastPage);
+                $summaryArray = explode('</span></span></span>', $pageArray[1]);
+              }
+            }
             // Make sure string is a city name
             // Run through weather scraper api?
             // call id weatherDescriptor and echo html to the div
           ?>
+          <div class="alert text-center" name="weatherDescription" id="weatherDescription">
+            <!-- Placeholder for code that describes weather conditions -->
+            <?php
+              // Add or remove class based on if there's data in $summaryArray
+              // Also, displays $summaryArray
+              if ($cityDisplay) {
+                $summaryArray = empty($summaryArray) ? "" : $summaryArray[0];
+                if ($summaryArray) {
+                  echo '<script type="text/javascript">';
+                  echo '$("#weatherDescription").addClass("alert-info");';
+                  echo '</script>';
+                  echo $summaryArray;
+                } elseif ($errorMessage) {
+                  echo '<script type="text/javascript">';
+                  echo '$("#weatherDescription").removeClass("alert-info");';
+                  echo '$("#weatherDescription").addClass("alert-danger");';
+                  echo '</script>';
+                  echo $errorMessage;
+                }
+              }
+            ?>
+          </div>
         </div> <!-- col-md-6 col-md-offset-3 -->
       </div> <!-- row -->
     </div> <!-- container -->
