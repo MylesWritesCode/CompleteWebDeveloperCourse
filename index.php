d95a2e58c444575269f93eaf8627954dc7abfeea
Myles Berueda <myles.berueda@gmail.com>
2016-12-28 13:24:40 -1000

    Connected to db using PHP

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+  $link = mysqli_connect("localhost", "cl25-users-6tr", "Mr-/EGFRY",  "cl25-users-6tr");
+  if (mysqli_connect_error()) {
+    die ("There was an error connecting to the database.");
+  }
+  $query = "SELECT * FROM users";
+
+  if ($result = mysqli_query($link, $query)) {
+    $row = mysqli_fetch_array($result);
+    print_r($row);
+    echo "Your email is ".$row['email'];
+    echo "Your password is ".$row['password'];
+  }
+?>
