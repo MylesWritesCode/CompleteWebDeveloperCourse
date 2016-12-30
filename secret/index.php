987ea6cfc9748d0c529823b0caf414ce4c6f9fa4
Myles Berueda <myles.berueda@gmail.com>
2016-12-29 14:56:34 -1000

    Added Sign Up feature
    Using PHP, created and connected site to an SQL server, with info saved on database.php file for security.

--- secret/index.php
+++ secret/index.php
@@ -1,6 +1,34 @@
 <?php
 include ("database.php");
-
+if (mysqli_connect_error()) {
+  die ("There was an error connecting to the database.");
+}
+// take email and password
+$email = empty($_POST["email"])? "" : $_POST["email"];
+$password = empty($_POST["password"])? "" : $_POST["password"];
+$message = "";
+//if email and password are entered
+if (!empty($email) && !empty($password)) {
+  // mysqli_real_escape_string vars
+  $email = mysqli_real_escape_string($link, $email);
+  $password = mysqli_real_escape_string($link, $password);
+  // query db for $email, set to $query
+  $query = "SELECT `id` FROM `users` WHERE `email` = '".$email."'";
+  // store results of query in $result
+  $result = mysqli_query($link, $query);
+  // if $result rows > 0
+  if (mysqli_num_rows($result) > 0) {
+    // warning: that email is already registered
+    $message = "<script type='text/javascript'>$('#alerts').removeClass('alert-info').addClass('alert-warning').html('That email address is already in use.'); $('#emailFormGroup').removeClass('has-error').addClass('has-warning');        $('#emailSpan').addClass('glyphicon-warning-sign').removeClass('glyphicon-ok');</script>";
+  } else {
+    // add user with password to db
+    $query = "INSERT INTO `users` (`email`, `password`) VALUES ('$email', '$password')";
+    mysqli_query($link, $query);
+    $message = "<script type='text/javascript'>$('#alerts').removeClass('alert-info alert-warning').addClass('alert-success').html('Submitted!');</script>";
+  }
+} else {
+  $message = "<script type='text/javascript'>$('#alerts').removeClass('alert-warning alert-error alert-success').addClass('alert-info').html('Fill out the form and click Sign Up!')</script>";
+}
 ?>
 
 <!DOCTYPE html>
@@ -33,12 +61,12 @@ include ("database.php");
                 <h4>Interested? Sign up now.</h4>
                 <br>
                 <div class="form-group has-feedback" id="emailFormGroup">
-                  <input class="form-control input-lg color" type="text" placeholder="Email Address" id="email" name="email">
+                  <input class="form-control input-lg color" type="text" placeholder="Email Address" id="email" name="email" value="<?php echo empty($_POST['email']) ? '' : $_POST['email']; ?>">
                   <span class="glyphicon form-control-feedback" aria-hidden="true" id="emailSpan"></span>
                   <span id="helpBlock1" class="help-block">Enter an email address.</span>
                 </div>
                 <div class="form-group has-feedback" id="passwordFormGroup">
-                  <input class="form-control input-lg color" type="password" placeholder="Password" id="password" name="password">
+                  <input class="form-control input-lg color" type="password" placeholder="Password" id="password" name="password" value="<?php echo empty($_POST['password'])? '' : $_POST['password']; ?>">
                   <span class="glyphicon form-control-feedback" aria-hidden="true" id="passwordSpan"></span>
                   <span id="helpBlock2" class="help-block">Enter your password.</span>
                 </div>
@@ -49,6 +77,7 @@ include ("database.php");
                   </label>
                 </div> <!-- checkbox -->
                 <div class="alert text-center notices" id="alerts">
+                  <?php echo $message ?>
                 </div>
                 <button class="btn btn-success btn-lg center-block" id="submitBtn">Sign Up</button>
                 <br>
