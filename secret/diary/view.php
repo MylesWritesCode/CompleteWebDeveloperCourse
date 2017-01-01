0bae1ce88dcfbd10e467c01883112b55a8235e12
Myles Berueda <myles.berueda@gmail.com>
2016-12-31 18:03:43 -1000

    Added edit.php and finished view.php

--- secret/diary/view.php
+++ secret/diary/view.php
@@ -1,14 +1,6 @@
 <?php
 include "../database.php";
 session_start();
-if (array_key_exists("id", $_SESSION)) {
-  // Code to bring up the posts you've created.
-} else {
-  header("Location: login.php");
-}
-
-// Start counting rows in `secrets`
-// find each secret by `id` in table `secret` while `user_id` == `session id`
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -19,20 +11,35 @@ if (array_key_exists("id", $_SESSION)) {
       <div class="row">
         <?php include "../sidebar.php"; ?>
         <div class="col-sm-6 col-sm-offset-1 element-ui">
-          <table class="table table-striped table-hover">
-            <tr>
-              <th>#</th>
-              <th>Title</th>
-              <th>Date</th>
-            </tr>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>First Post</td>
-                <td>12-1-2016</td>
-              </tr>
-            </tbody>
-          </table>
+          <br>
+          <div class="panel panel-default">
+            <!-- Default panel contents -->
+            <div class="panel-heading"><h1 class="text-center">Your Secrets</h1></div>
+            <div class="list-group">
+              <?php
+              if (array_key_exists("id", $_SESSION)) {
+                // Start bringing up posts the user created
+                $userId = $_SESSION['id'];
+                // Query db for all posts created by user
+                $query = "SELECT `date`, `title`, `id` FROM `secrets` WHERE `user_id` ='".$userId."'";
+                if ($result = mysqli_query($link, $query)) {
+                  while ($row = mysqli_fetch_row($result)) {
+                    $date = $row [0];
+                    $title = $row[1];
+                    $id = $row[2];
+                    echo "<a href='edit.php?".$id."' class='list-group-item'>";
+                    echo "<span class='badge'>".$date."</span>".$title;
+                    echo "</a>";
+                  }
+                }
+                // Close SQL connection
+                mysqli_close($link);
+              } else {
+                header("Location: login.php");
+              }
+              ?>
+            </div>
+          </div>
         </div> <!-- col-md-6 col-md-offset-3 -->
       </div> <!-- row -->
     </div> <!-- container-fluid -->
