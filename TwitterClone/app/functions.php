a766e92bec897b7ad931ad128ab4887f1a045015
Myles Berueda <myles.berueda@gmail.com>
2017-01-03 01:49:14 -1000

    Added tweet display, search, and post tweet inputs
    Doesn't look like much right now, but I'm just focusing on the functionality of the website before making it look good.

--- TwitterClone/app/functions.php
+++ TwitterClone/app/functions.php
@@ -1,10 +1,75 @@
 <?php
   session_start();
+  include "database.php";
   if (array_key_exists("function", $_GET)) {
     if ($_GET["function"] == "logout") {
       $_SESSION = array();
       session_destroy();
       header("Location: index.php");
     }
+  }
+
+  function timeSince($since) {
+    $chunks = array(
+      array(60 * 60 * 24 * 365, 'year'),
+      array(60 * 60 * 24 * 30, 'month'),
+      array(60 * 60 * 24 * 7, 'week'),
+      array(60 * 60 * 24, 'day'),
+      array(60 * 60, 'hour'),
+      array(60, 'minute'),
+      array(1, 'second'),
+    );
+    for ($i = 0, $j = count($chunks); $i < $j; $i++) {
+      $seconds = $chunks[$i][0];
+      $name = $chunks[$i][1];
+      if (($count = floor($since/$seconds)) != 0) {
+        break;
+      }
+    }
+    $print = ($count == 1) ? '1 '.$name : "$count {$name}s";
+    return $print;
+  }
+  // Display Tweets
+  function displayTweets($type) {
+    global $link;
+
+    if ($type == 'public') {
+      $whereClause = "";
+    }
+
+    $query = "SELECT * FROM `tweets` ".$whereClause." ORDER BY `datetime` DESC LIMIT 10";
+    $result = mysqli_query($link, $query);
+
+    if (mysqli_num_rows($result) == 0) {
+      echo "There are no tweets to display.";
+    } else {
+      while ($row = mysqli_fetch_assoc($result)) {
+        $userQuery = "SELECT * FROM `users` WHERE `id` = '".mysqli_real_escape_string($link, $row['user_id'])."'LIMIT 1";
+        $userQueryResult = mysqli_query($link, $userQuery);
+        $user = mysqli_fetch_assoc($userQueryResult);
+        echo "<div class='tweet'>";
+        echo "<p>".$user['email']."<span class='time'> - ".timeSince(time() - strtotime($row['datetime']))." ago</span>:</p>";
+        echo "<p>".$row['tweet']."</p>";
+        echo "<p>Follow</p>";
+        echo "</div>";
+      }
+    }
+  }
+
+  // Display Search
+  function displaySearch() {
+    echo "<div class='form-group form-inline'>
+            <input type='text' placeholder='Search Tweets' class='form-control'>
+            <button class='btn btn-primary'>SEARCH</button>
+          </div>";
+  }
+
+  // Display Post Tweet
+  function displayTweetBox() {
+    echo "<form method='POST'>
+            <textarea class='form-control' rows='6'></textarea>
+            <br>
+            <button class='btn btn-primary btn-block'>POST TWEET</button>
+          </form>";
   }
 ?>
--- TwitterClone/app/views/home.php
+++ TwitterClone/app/views/home.php
@@ -1,6 +1,13 @@
 <div class="container-fluid">
-  <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-    </div> <!-- col-md-8 col-md-offset-2 -->
+  <div class="row margin-top-20px">
+    <div class="col-md-8">
+      <h2>Recent Tweets</h2>
+      <?php displayTweets('public'); ?>
+    </div> <!-- col-md-8 -->
+    <div class="col-md-4">
+      <?php displaySearch(); ?>
+      <hr>
+      <?php displayTweetBox(); ?>
+    </div> <!-- col-md-4 -->
   </div> <!-- row -->
 </div> <!-- container-fluid -->
